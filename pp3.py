2e8351da1974e50fd4dbd8ce51c18a833bbaec0f
Jailson Pereira <jailsoncolares@gmail.com>
2018-05-18 13:16:29 -0400

    agora vai?

--- pp3.py
+++ pp3.py
@@ -27,26 +27,31 @@ def q4():
 	global cont1
 	global cont2
 	global fita3
-	if((fita1[cont1] == '0' and fita2[cont2] == '0') or (fita1[cont1] == '1' and fita2[cont2]== '0') or (fita1[cont1] == '0' and fita2[cont2] == '1')):
-		fita3.append(1)
+	if((fita1[cont1] == '0' and fita2[cont2] == '0')):
+		fita3.append('1')
+		cont1 = cont1-1
+		cont2 = cont2-1
+		q3()
+	elif((fita1[cont1] == '1' and fita2[cont2]== '0') or (fita1[cont1] == '0' and fita2[cont2] == '1')):
+		fita3.append('1')
 		cont1 = cont1-1
 		cont2 = cont2-1
 		q3()
 	elif(fita1[cont1] == '1' and fita2[cont2] == '1'):
-		fita3.append(0)
+		fita3.append('1')
 		cont1 = cont1 -1
 		cont2 = cont2-1
 		q4()
 	elif(fita1[cont1] == ' ' and fita2[cont2] == ' '):
-		fita3.append(1)
+		fita3.append('1')
 		q3()
 def q5():
-	#print(fita3,"ACEITA")
+	
 	global fita3
 	msg = ""
 	for i in range(1,len(fita3)):
 		msg = msg+str(fita3[i])
-	#print(fita3)
+	
 	print(msg[::-1],"ACEITA")
 
 def q3():
@@ -54,8 +59,13 @@ def q3():
 	global cont2
 	global fita3
 	if(len(fita1) == len(fita2)): #fitas de tamanho igual
-		if((fita1[cont1] == '0' and fita2[cont2] == '0') or (fita1[cont1] == '1' and fita2[cont2] == '0') or (fita1[cont1] == '0' and fita2[cont2] == '1') ):
+		if((fita1[cont1] == '0' and fita2[cont2] == '0')):
 			fita3.append('0')
+			cont1 = cont1-1
+			cont2 = cont2-1
+			return q3()
+		elif((fita1[cont1] == '1' and fita2[cont2] == '0') or (fita1[cont1] == '0' and fita2[cont2] == '1') ):
+			fita3.append('1')
 			cont1 = cont1-1
 			cont2 = cont2-1
 			return q3()
@@ -66,27 +76,38 @@ def q3():
 			return q4()
 
 		return q5()
-	'''elif(len(fita1) != len(fita2)):
+	elif(len(fita1) != len(fita2)):
 		#print("entrou")
-		#print(cont1)
-		#print(cont2)
-		if(len(fita1) > len(fita2)):
-			if((fita1[cont1] == '0' and fita2[cont2] == '0') or (fita1[cont1] == '1' and fita2[cont2] == '0') or (fita1[cont1] == '0' and fita2[cont2] == '1') ):
-				fita3.append('0')
+		if((fita1[cont1] == '0' and fita2[cont2] == '0')):
+			fita3.append('0')
+			cont1 = cont1-1
+			cont2 = cont2-1
+			return q3()
+		elif((fita1[cont1] == '1' and fita2[cont2] == '0') or (fita1[cont1] == '0' and fita2[cont2] == '1') ):
+			fita3.append('1')
+			cont1 = cont1-1
+			cont2 = cont2-1
+			return q3()
+		elif(fita1[cont1] == '1' and fita2[cont2] == '1'):
+			fita3.append('0')
+			cont1 = cont1-1
+			cont2 = cont2-1
+			return q4()
+		elif((fita1[cont1] == '1' and fita2[cont2] == ' ') or (fita1[cont1]=='0' and fita2[cont2]==' ') or (fita2[cont2] == '1' and fita1[cont1] == ' ') or (fita2[cont2] == '0' and fita1[cont1] == ' ')):
+			if(cont1 > cont2):
+				print("entrou")
+				fita3.append(fita1[cont1])
 				cont1 = cont1-1
+				return q3()
+			else:
+				fita3.append(fita2[cont2])
 				cont2 = cont2-1
 				return q3()
-			elif(fita1[cont1] == '1' and fita2[cont2] == '1'):
-				fita3.append('0')
-				cont1 = cont1-1
-				cont2 = cont2-1
-				return q4()
-			else:
-				for i in range(cont1):
-					#print(fita3[i])
-					fita3.append(fita1[i])
-				return q5()
-		'''
+
+		return q5()
+				
+				
+		
 
 def inicializa_fita(fita_entrada):
 	indice = 0
@@ -103,8 +124,6 @@ def inicializa_fita(fita_entrada):
 		fita1.append(' ') #adiciona um espaço em branco ao final da lista
 		q1(fita_entrada,indice+1,len(fita_entrada))
 		fita2.append(' ') #adiciona um espaço em branco ao final da lista
-		#print(fita1)
-		#print(fita2)
 		q3()
 
 entrada =entrada()
